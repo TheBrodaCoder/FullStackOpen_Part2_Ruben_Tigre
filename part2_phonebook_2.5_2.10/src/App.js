e1f36998a7e1207d7054f8b1dc06c018f2a87555
Ruben <donowenrt@gmail.com>
2021-03-28 18:39:07 +0200

    avoid to add the same name

--- part2_phonebook_2.5_2.10/src/App.js
+++ part2_phonebook_2.5_2.10/src/App.js
@@ -1,13 +1,50 @@
+import React, { useState } from 'react'
 
-function App() {
-  
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]);
+  const [ newName, setNewName ] = useState('');
+  const [ error, setError] = useState('');
 
+  const handleChange = (evt) => {
+    setNewName(evt.target.value);
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const noteObject = {
+      name: newName
+    };
+    persons.some(person => person.name === newName) ? 
+    (alert(`${newName} is already added to phonebook`))
+    :
+    (newName === '' ? setError('Introduzca un nombre') : (setPersons(persons.concat(noteObject), setError('') ) ) );
+    setNewName('');
+  };
+
+  const renderedNumbers = persons.map(
+    person => <li key={person.name + '_' + persons.length}>{person.name}</li>
+  )
 
   return (
-    <div className="App">
-
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input onChange={handleChange} value={newName}/>
+          <p>{error}</p>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {renderedNumbers}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
