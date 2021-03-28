69d9d60b991ef740a004e22048632d3d43b07146
Ruben <donowenrt@gmail.com>
2021-03-28 13:06:53 +0200

    Handle arbitrary number of courses

--- part2_courseinfo_2.1_2.4/src/App.js
+++ part2_courseinfo_2.1_2.4/src/App.js
@@ -4,29 +4,54 @@ import Content from './Content';
 import Total from './Total';
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const courses =  [
+    {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
+    },
+    {
+      name: 'Half Stack application development 2',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
+    }
+  ];
+
+  let coursesToRender = courses.map(course =>
+    <div key={course.name}>
+      <Header key={course.name + '_header'} course={course} />
+      <Content key={course.name + '_content'} course={course} />
+      <Total key={course.name + '_total'} course={course} />
+    </div>
+    )
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      {coursesToRender}
     </div>
   )
 }
